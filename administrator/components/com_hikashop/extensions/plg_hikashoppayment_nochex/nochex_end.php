e965cb4d92e09c9cfd6296f438b5f3949472b579
NochexDevTeam <36759878+NochexDevTeam@users.noreply.github.com>
2018-07-25 16:15:38 +0100

    Create nochex_end.php

---
+++ administrator/components/com_hikashop/extensions/plg_hikashoppayment_nochex/nochex_end.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+
+ * @package	HikaShop for Joomla!
+
+ * @version	3.5.1
+
+ * @author	hikashop.com
+
+ * @copyright	(C) 2010-2018 HIKARI SOFTWARE. All rights reserved.
+
+ * @license	GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+
+ */
+
+defined('_JEXEC') or die('Restricted access');
+
+?><div class="hikashop_nochex_end" id="hikashop_nochex_end" style="text-align:center;">
+
+	<span id="hikashop_nochex_end_message" class="hikashop_nochex_end_message">
+
+		<?php echo JText::sprintf('PLEASE_WAIT_BEFORE_REDIRECTION_TO_X',$this->payment_name).'<br/>'. JText::_('CLICK_ON_BUTTON_IF_NOT_REDIRECTED');?>
+
+	</span>
+<br/><br/>
+	<span id="hikashop_nochex_end_spinner" class="hikashop_nochex_end_spinner">
+
+		<img src="<?php echo HIKASHOP_IMAGES.'spinner.gif';?>" />
+
+	</span>
+
+<br/><br/>
+	<br/>
+
+	<form id="hikashop_nochex_form" name="hikashop_nochex_form" action="https://secure.nochex.com/default.aspx" method="post">
+
+		<div id="hikashop_nochex_end_image" class="hikashop_nochex_end_image">
+
+			<input id="hikashop_nochex_button" type="submit" class="btn btn-primary" value="<?php echo JText::_('PAY_NOW');?>" name="" alt="<?php echo JText::_('PAY_NOW');?>" />
+
+		</div>
+
+		<?php
+
+			foreach( $this->vars as $name => $value ) {
+
+				echo '<input type="hidden" name="'.$name.'" value="'.htmlspecialchars($value).'" />';
+
+			}
+
+			$doc = JFactory::getDocument();
+
+			$doc->addScriptDeclaration("window.hikashop.ready( function() {document.getElementById('hikashop_nochex_form').submit();});");
+
+			hikaInput::get()->set('noform',1);
+
+		?>
+
+	</form>
+
+</div>
+
